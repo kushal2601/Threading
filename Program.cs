b1e70c9f5e3c7a7cb4c3656f37fa61b9f3c27d41
Kushal Maradana <Kushal.Maradana@leoforce.local>
2023-08-01 00:31:19 +0530

    Passing arguments to the Threads using lambda expressions

--- Program.cs
+++ Program.cs
@@ -2,22 +2,24 @@
 using System.Threading;
 using System.Diagnostics;
 
-static void Download(object? fileName){
-    if (fileName == null)return;
-
+static void Download(string fileName){
     string bookName = (string) fileName;
     Console.WriteLine($"Downloading {bookName} ");
     Thread.Sleep(1000);
     Console.WriteLine($"{bookName} Downloaded");
 }
 
-var thread1 = new Thread(Download);
-var thread2 = new Thread(Download);
+var thread1 = new Thread(()=>{
+    Download("git scm book.pdf");
+});
+var thread2 = new Thread(()=>{
+    Download("C++ by Walter Savitch.pdf");
+});
 
 var watch =  Stopwatch.StartNew();
 
-thread1.Start("git scm book.pdf");
-thread2.Start("C++ by Walter Savitch.pdf");
+thread1.Start();
+thread2.Start();
 
 thread1.Join();
 thread2.Join();
