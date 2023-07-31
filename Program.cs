5e673de623c6f411f2a1a83936f3055a32c343f3
Kushal Maradana <Kushal.Maradana@leoforce.local>
2023-07-31 20:02:27 +0530

    Created two basic threads to understand how they work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using System.Threading;
+using System.Diagnostics;
+
+void Download(){
+    Console.WriteLine("Download started");
+    Thread.Sleep(1000);
+    Console.WriteLine("Download finished");
+}
+
+var thread1 = new Thread(Download);
+var thread2 = new Thread(Download);
+
+var watch =  Stopwatch.StartNew();
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+watch.Stop();
+
+Console.WriteLine($"It took {watch.Elapsed.Seconds} seconds to finish the process ");
